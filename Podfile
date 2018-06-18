2d97046472388aeeec423a19b0c46974d2745281
Andrew Crookston <andrew@caoos.com>
2018-06-17 17:28:22 -0700

    Remove CocoaPods

--- Podfile
+++
@@ -1,5 +0,0 @@
-platform :macos, '10.12.0'
-
-target 'Pretty JSON' do
-  use_frameworks!
-end
--- Pods/Target Support Files/Pods-Pretty JSON/Pods-Pretty JSON-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pretty_JSON : NSObject
-@end
-@implementation PodsDummy_Pods_Pretty_JSON
-@end
